0904ab366d6ac62a47f50d7839489797eabc78e8
Angie Leiva <38085631+ancarolm@users.noreply.github.com>
2019-08-05 20:51:07 -0600

    Versión 5/8/19 - 8:50

--- IUApp/Factura.cs
+++ IUApp/Factura.cs
@@ -52,51 +52,9 @@ namespace IUApp
 
         private void button2_Click(object sender, EventArgs e)
         {
-            DataTable dt = new DataTable();
-
-            try
-            {
-                C.idPlatillo = Convert.ToInt32(cbxCategoria.SelectedValue);
-                dt = V.AgregarVenta(C.idPlatillo);
-                dataGridView1.Rows.Clear();
-                for (int i = 0; i < dt.Rows.Count; i++)
-                {
-                    dataGridView1.Rows.Add(dt.Rows[i][0]);
-                    dataGridView1.Rows[i].Cells[0].Value = dt.Rows[i][0].ToString();
-                    dataGridView1.Rows[i].Cells[1].Value = dt.Rows[i][1].ToString();
-                    dataGridView1.Rows[i].Cells[2].Value = dt.Rows[i][2].ToString();
-                    dataGridView1.Rows[i].Cells[3].Value = dt.Rows[i][3].ToString();
-                    dataGridView1.Rows[i].Cells[4].Value = dt.Rows[i][4].ToString();
-                    //dataGridView1.Rows[i].Cells[5].Value = dt.Rows[i][5].ToString();
-                    //dataGridView1.Rows[i].Cells[6].Value = dt.Rows[i][6].ToString();
-                }
-
-            }
-            catch (Exception ex)
-            {
-                DevComponents.DotNetBar.MessageBoxEx.Show(ex.Message);
-            }
+            
         }
 
-
-        private void LlenarGrilla()
-        {
-            Decimal SumaSubTotal = 0; Decimal SumaIgv = 0; Decimal SumaTotal = 0;
-            dataGridView1.Rows.Clear();
-            for (int i = 0; i < lst.Count; i++)
-            {
-                dataGridView1.Rows.Add();
-                dataGridView1.Rows[i].Cells[0].Value = lst[i].IdFactura;
-                dataGridView1.Rows[i].Cells[1].Value = lst[i].IdCliente;
-                dataGridView1.Rows[i].Cells[2].Value = lst[i].IdVendedor;
-                dataGridView1.Rows[i].Cells[3].Value = lst[i].Detalle;
-                dataGridView1.Rows[i].Cells[5].Value = lst[i].Precio;
-                dataGridView1.Rows[i].Cells[6].Value = lst[i].SubTotal;
-                SumaSubTotal += Convert.ToDecimal(dataGridView1.Rows[i].Cells[4].Value);
-                SumaIgv += Convert.ToDecimal(dataGridView1.Rows[i].Cells[6].Value);
-            }
-
-        }
 
         private void button4_Click(object sender, EventArgs e)
         {
