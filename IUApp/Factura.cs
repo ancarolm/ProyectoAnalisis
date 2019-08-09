427219040c15ae46f7523078ade46079082c40ed
ManuCL <43527050+ManuCL@users.noreply.github.com>
2019-08-08 20:43:45 -0600

    Update Factura.cs
    Funcion de boton 2

--- IUApp/Factura.cs
+++ IUApp/Factura.cs
@@ -52,7 +52,7 @@ namespace IUApp
             comboBox.DataSource = V.Listado();
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e)// Boton de agregar algun producto a la factura
         {
             Venta V = new Venta();
 
